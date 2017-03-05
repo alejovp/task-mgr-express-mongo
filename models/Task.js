9ff0dadab218daec0e71ff418d0c81f02550b6a8
Alejandro Vazquez <alejovp@gmail.com>
2017-03-06 00:22:56 +0100

    package.json updated

--- models/Task.js
+++ models/Task.js
@@ -2,8 +2,6 @@ const mongoose = require('mongoose')
 const collection = 'tasks'
 
 const TaskSchema = new mongoose.Schema({
-  // firstName: { type: String, required: true },
-  // lastName: { type: String, required: true },
   title: String,
   description: String,
   done: Boolean,
